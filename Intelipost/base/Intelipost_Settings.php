dd198e05a31dcebd7cd1863291b76fcd35aa2a4b
stefanrehm <stefan.rehm@project-a.com>
2014-08-20 23:51:29 -0300

    added php & sdk version

--- Intelipost/base/Intelipost_Settings.php
+++ Intelipost/base/Intelipost_Settings.php
@@ -5,6 +5,10 @@
  */
 
 class Intelipost_Settings {
+    
+    const PHP_VERSION = phpversion();
+    const SDK_VERSION = "0.3.4";
+    
     const API_URL = "https://api.intelipost.com.br/api/v1";
     const API_KEY = "d0a3b5f355e17333de5c9a15d665f3d7724b4246540012d5eb851a82db1b95a5";
 
